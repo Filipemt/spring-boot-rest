64fed6d8b16c050a71fdcf9e7d3c413ee4eed3f4
Filipe Mota <filipera3@gmail.com>
2026-02-25 20:57:38 -0300

    database: adicionando migration com flyway

---
+++ src/main/resources/db/migration/V2__insert_table_person.sql
@@ -0,0 +1,5 @@
+INSERT INTO person(id, first_name, last_name, address, gender) VALUES
+                                                                   (1, 'Filipe', 'Mota', 'Distrito Federal - Brasília - Brasil', 'M'),
+                                                                   (2, 'Juraci', 'Amélia', 'Patos - Minas Gerais - Brasil', 'F'),
+                                                                   (3, 'Emanuelle', 'Barbosa', 'Distrito Federal - Brasília - Brasil', 'F'),
+                                                                   (4, 'Fábio', 'Pires', 'Distrito Federal - Brasília - Brasil', 'M')
